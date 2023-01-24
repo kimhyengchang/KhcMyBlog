367a09d518c15956d7d02cdc11f6f1ad41acec9b
kimhyengchang <woesf21@gmail.com>
2023-01-24 11:13:17 +0900

    2023-01-24 메인 화면 컴포넌트화

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import React, { useState } from "react";
 import Sidebar from './components/sidebar/Sidebar'
 import Topbar from './components/topbar/Topbar';
+import Home from './pages/home/Home'
 function App() {
   return (
       <div className='App'>
         <Topbar/>
           <div className="container">
             <Sidebar />
-            <div className="others">other pages</div>
+            <Home />
           </div>
       </div>
   );
